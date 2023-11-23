1b9af87162d89f3adb71bb9472ea3e00b4fc68f2
Matej Macko <macik0009@gmail.com>
2023-11-23 21:32:39 +0100

    framework

---
+++ App/Views/Home/beer.view.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var Array $data */
+
+/** @var \App\Core\LinkGenerator $link */
+?>
+
+<div class="container-beer container px-4 py-5 px-lg-5 my-5">
+    <div class="row gx-4 gx-lg-5 align-items-center">
+        <div class="col-md-6">
+            <img class="mx-auto d-block" src="public/images/sell/pivo1.png" alt="beer">
+        </div>
+        <div class="col-md-6">
+            <h1 class="fw-bolder mt-4">BACK IN TOWN</h1>
+            <div class="fs-5 mb-5">
+                <h3>Cena: 5 EUR</h3>
+            </div>
+            <p class="lead mb-4 pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+            <div class="d-flex">
+                <input class="form-control text-center me-3" id="beerQuantity" type="num" value="1">
+                <button type="button" class="btn btn-sm btn-outline-secondary btn-custom">Pridať do košíka</button>
+            </div>
+        </div>
+    </div>
+</div>
+
