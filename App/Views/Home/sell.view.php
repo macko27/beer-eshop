4bf18af582ff8e0ceed3db8d677fe1af67635b93
Matej Macko <macik0009@gmail.com>
2023-11-25 22:26:28 +0100

    neviem

--- App/Views/Home/sell.view.php
+++ App/Views/Home/sell.view.php
@@ -26,59 +26,66 @@
         </form>
     </div>
 
-    <section>
-        <div class="sidebar">
-            <h5 class="sidebar-title">Pivovar</h5>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="ZRP">
-                <label class="form-check-label" for="ZRP">
-                    Zvolenský remeselný pivovar
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="vrsky">
-                <label class="form-check-label" for="vrsky">
-                    Vŕšky
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="baran">
-                <label class="form-check-label" for="baran">
-                    Baran
-                </label>
-            </div>
-        </div>
-        <div class="sidebar">
-            <h5 class="sidebar-title">Druh</h5>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="tmave">
-                <label class="form-check-label" for="tmave">
-                    Tmavé
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="polotmave">
-                <label class="form-check-label" for="polotmave">
-                    Polotmavé
-                </label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="svetle">
-                <label class="form-check-label" for="svetle">
-                    Svetlé
-                </label>
-            </div>
-        </div>
-    </section>
+    <aside class="bd-aside sticky-xl-top text-body-secondary align-self-start mb-3 mb-xl-5 px-2">
+        <h2 class="h6 pt-4 pb-3 mb-4 border-bottom">On this page</h2>
+        <nav class="small" id="toc">
+            <ul class="list-unstyled">
+                <li class="my-2">
+                    <button class="btn d-inline-flex align-items-center border-0 collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#contents-collapse" aria-controls="contents-collapse">Contents</button>
+                    <ul class="list-unstyled ps-3 collapse" id="contents-collapse" style="">
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#typography">Typography</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#images">Images</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#tables">Tables</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#figures">Figures</a></li>
+                    </ul>
+                </li>
+                <li class="my-2">
+                    <button class="btn d-inline-flex align-items-center border-0 collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#forms-collapse" aria-controls="forms-collapse">Forms</button>
+                    <ul class="list-unstyled ps-3 collapse" id="forms-collapse" style="">
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#overview">Overview</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#disabled-forms">Disabled forms</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#sizing">Sizing</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#input-group">Input group</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#floating-labels">Floating labels</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#validation">Validation</a></li>
+                    </ul>
+                </li>
+                <li class="my-2">
+                    <button class="btn d-inline-flex align-items-center border-0 collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#components-collapse" aria-controls="components-collapse">Components</button>
+                    <ul class="list-unstyled ps-3 collapse" id="components-collapse" style="">
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#accordion">Accordion</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#alerts">Alerts</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#badge">Badge</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#breadcrumb">Breadcrumb</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#buttons">Buttons</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#button-group">Button group</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#card">Card</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#carousel">Carousel</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#dropdowns">Dropdowns</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#list-group">List group</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#modal">Modal</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#navs">Navs</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#navbar">Navbar</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#pagination">Pagination</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#popovers">Popovers</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#progress">Progress</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#scrollspy">Scrollspy</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#spinners">Spinners</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#toasts">Toasts</a></li>
+                        <li><a class="d-inline-flex align-items-center rounded text-decoration-none" href="#tooltips">Tooltips</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </aside>
 
 
     <div class="album py-5">
-        <div class="container-sell">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-5">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-5">
                 <?php foreach ($data["beers"] as $beer): ?>
                         <div class="col">
                             <a href="<?= $link->url("home.beer", ["id" => $beer->getId()]) ?>">
-                            <input type="checkbox">
                             <div class="card mx-auto border-0">
                                     <img src="<?= $beer->getObrazok() ?>" alt="pivo">
                                 <div class="card-body">
